4e2c8bb393b795987d49e7426f1fe7f34ec67630
Connor McNaboe <mcnaboeconnor@gmail.com>
2019-03-12 10:05:58 -0400

    Modified: Calculation logic

--- app/fields.py
+++ app/fields.py
@@ -6,7 +6,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class CaclFields(FlaskForm):
+class CalcFields(FlaskForm):
     integer1 = IntegerField('Enter Integer 1', validators=[DataRequired()])
     integer2 = IntegerField('Enter Integer 2', validators=[DataRequired()])
     add = SubmitField('+')
--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,35 @@
 # Author: Connor McNaboe
 # Purpose: Sets routes/urls to be requested by usesrs
 
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for
 from app import app
-from app.fields import CaclFields
+from app.fields import CalcFields
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = CaclFields()
-    result = '' 
+    form = CalcFields()
+    answer = ''
     if form.validate_on_submit():
-        
-    return render_template('index.html', title='Web Calc Home', form=form)
+        int1 = form.integer1.data
+        int2 = form.integer2.data
+        # Evaluate input integers
+        if form.add.data:
+            # Addition
+            answer = str(int1 + int2)
+        elif form.subtract.data: 
+            # Subtraction
+            answer = str(int1 - int2)
+        elif form.multiply.data: 
+            # Multiplicaiton
+            answer = str(int1*int2)
+        elif form.divide.data:
+            # Division
+            answer = str(int1 / int2)
+        elif form.clear.data: 
+            # Clear results and integers
+            answer = ''
+            form.integer1.data = ''
+            form.integer2.data = ''
+            return redirect(url_for('index'))
+    return render_template('index.html', title='Web Calc Home', form=form, answer=answer)
